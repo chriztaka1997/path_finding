1e0f21ae2085867e9adcf6e98b146e9c6e1d0bff
Christopher Imantaka <chris.imantaka1997@gmail.com>
2020-12-11 09:25:57 -0800

    second commit

--- src/main/visualizer.jsx
+++ src/main/visualizer.jsx
@@ -3,9 +3,10 @@ import node from './node/node';
 
 import './visualizer.csx';
 
-export default class visualizer extends Component{
+export default class Visualizer extends Component{
     constructor(props){
-
+        super.props(props);
+        this.state = {};
     }
 
     render(){
